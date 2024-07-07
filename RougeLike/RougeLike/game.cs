18c025befe5584f6f8ce0a9d4c1359da83657d2d
Giovanni Peirone <mgpeirone@escuelasproa.edu.ar>
2024-07-07 20:03:23 -0300

    a

---
+++ RougeLike/RougeLike/game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RougeLike
+{
+    class game
+    {
+        public void run()
+        {
+
+        }
+    }
+}
